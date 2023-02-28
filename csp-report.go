82be9178579c742a130fbdba71ffdc3bf906e753
Robbie <txt3rob@gmail.com>
2023-02-28 13:19:58 +0000

    Update csp-report.go

--- csp-report.go
+++ csp-report.go
@@ -11,7 +11,8 @@ import (
 )
 
 
-var domainRegex = regexp.MustCompile(`([a-zA-Z0-9-]+)\.([a-z0-9-]+)?\.?s3(\.[a-z0-9-]+)?\.amazonaws\.com|blob\.core\.windows\.net|storage\.googleapis\.com`)
+var domainRegex = regexp.MustCompile(`([a-zA-Z0-9-]+)\.([a-z0-9-]+)?\.?s3(?:[-.][a-z0-9-]+)*\.amazonaws\.com|blob\.core\.windows\.net|storage\.googleapis\.com`)
+
 
 func processURL(url string) {
     // Make the request
